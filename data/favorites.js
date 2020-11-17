6b79e9da97d20983615a5c82eb50d7848660c003
sarah-svg <kjskillen@gmail.com>
2020-11-16 16:07:01 -0800

    table name

--- data/favorites.js
+++ data/favorites.js
@@ -2,13 +2,19 @@ module.exports = [
   {
     videoId: 'T9AWDVVad00',
     title: 'Diplo ft. Morgan Wallen - Heartless (Karaoke Version)',
+    thumbnails: 'hey',
+    owner_id: 1,
   },
   {
     videoId: '9rnQFsbDa3o',
     title: 'Frozen 2 - Into The Unknown (Karaoke Version)',
+    thumbnails: 'hey',
+    owner_id: 1,
   },
   {
     videoId: 'GOFxPxTQUvY',
     title: 'Salem Ilese - Mad At Disney (Karaoke Version)',
+    thumbnails: 'hey',
+    owner_id: 1,
   }
 ];
