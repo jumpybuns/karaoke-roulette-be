9bb699b02dac246a92dfe846a2c65d98c9c847b0
Jumpybuns <ethanpiercepresents@gmail.com>
2020-11-18 11:45:16 -0800

    functions

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,31 +10,31 @@ const { mungedVideos } = require('../utils.js');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token;
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns favorites', async() => {
+    test('returns favorites', async () => {
       const mungedData = mungedVideos(data);
-      expect(mungedData).toEqual( 
+      expect(mungedData).toEqual(
         [
           {
             'thumbnails': 'https://i.ytimg.com/vi/1Y77BelxUQY/default.jpg',
@@ -48,11 +48,26 @@ describe('app routes', () => {
           }
         ]
       );
+    });
+    test('post to faves', async () => {
+      const expectation = {
+        'thumbnails': 'https://i.ytimg.com/vi/E0id9kAMS4k/default.jpg',
+        'title': 'Dan + Shay - Tequila (Karaoke Version)',
+        'videoid': 'E0id9kAMS4k',
+        'owner_id': 2,
+        'id': 2
+      };
+      const favorites = {
+        'thumbnails': 'https://i.ytimg.com/vi/E0id9kAMS4k/default.jpg',
+        'title': 'Dan + Shay - Tequila (Karaoke Version)',
+        'videoId': 'E0id9kAMS4k',
+      };
+      const data = await fakeRequest(app)
+        .post('/api/favorites')
+        .send(favorites)
+        .set('Authorization', token);
 
-
-
-
-
+      expect(data.body).toEqual(expectation);
     });
   });
 });
